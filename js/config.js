54fbc8c349865cd2328737ddc005617b64f9f145
Nishithkrishnan <nishithmanu@gmail.com>
2025-01-08 18:10:38 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', ap-southeast-2_Qm5pQJt1h // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', 3dak3n7vh4dspnfb0ijhtd1kbu // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' ap-southeast-2// e.g. us-east-2
+        userPoolId: 'ap-southeast-2_Qm5pQJt1h',  
+        userPoolClientId: '3dak3n7vh4dspnfb0ijhtd1kbu',  // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-southeast-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
